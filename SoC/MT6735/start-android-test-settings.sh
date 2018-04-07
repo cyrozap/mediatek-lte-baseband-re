65c13718587d96dc7608f2773386f48ebb5e0d4d
cyrozap <cyrozap@gmail.com>
2018-04-07 00:30:21 -0500

    Add scripts to start modem test/debug apps

---
+++ SoC/MT6735/start-android-test-settings.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+adb shell am start -n com.android.settings/com.android.settings.TestingSettings
---
+++ SoC/MT6735/start-engineer-mode.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+adb shell am start -n com.mediatek.engineermode/com.mediatek.engineermode.EngineerMode
