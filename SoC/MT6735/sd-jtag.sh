1f0dc8bd910674e25caf28720655524364cbd484
cyrozap <cyrozap@gmail.com>
2018-04-07 00:16:44 -0500

    Add some documentation for the SD-to-JTAG mux-change script

--- SoC/MT6735/sd-jtag.sh
+++ SoC/MT6735/sd-jtag.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+#
+# Reconfigure the pinmux for the SD card pins (MSDC1) to enable JTAG.
+# Note that these pins can also be used for SWD, with TMS and TCK as
+# SWDIO and SWCLK respectively.
+#
+# Pin mapping:
+# +------+-------+
+# |  SD  |  JTAG |
+# |------+-------|
+# | DAT1 |  TDO  |
+# | DAT0 |  TDI  |
+# | CLK  |  TCK  |
+# | CMD  |  TMS  |
+# +------+-------+
+#
 
 adb shell su -c /data/local/tmp/poke 0x10211400 0x6db11249
