1c3790ef3a89ac7bcb9d56e0db7298b0093b845b
cyrozap <cyrozap@gmail.com>
2018-04-29 22:17:55 -0500

    Add field names

--- SoC/MT6735/modemfw/make_img.py
+++ SoC/MT6735/modemfw/make_img.py
@@ -6,23 +6,25 @@ from datetime import datetime
 
 
 def gen_footer(data):
-    f = b'CHECK_HEADER'
-    f += struct.pack('<I', 3)
-    f += struct.pack('<I', 2)
-    f += struct.pack('<I', 5)
-    f += struct.pack('16s', b'MT6735_S00')
-    f += struct.pack('64s', datetime.utcnow().strftime("%Y/%m/%d %H:%M").encode('utf-8'))
-    f += struct.pack('64s', b'')
-    f += struct.pack('B', 1)
-    f += struct.pack('B', 0)
-    f += b'\0\0'
-    f += struct.pack('<I', 0x03000000)
-    f += struct.pack('<I', len(data))
-    f += struct.pack('<I', 0)
-    f += struct.pack('<I', 0x00A20000)
-    f += struct.pack('<I', 0x00219D1C)
-    f += b'\0' * 88
-    f += struct.pack('<I', 284)
+    # drivers/misc/mediatek/include/mt-plat/mt_ccci_common.h
+    # md_check_header_v3
+    f = b'CHECK_HEADER'  # check_header
+    f += struct.pack('<I', 3)  # header_verno
+    f += struct.pack('<I', 2)  # product_ver
+    f += struct.pack('<I', 5)  # image_type
+    f += struct.pack('16s', b'MT6735_S00')  # platform
+    f += struct.pack('64s', datetime.utcnow().strftime("%Y/%m/%d %H:%M").encode('utf-8'))  # build_time
+    f += struct.pack('64s', b'')  # build_ver
+    f += struct.pack('B', 1)  # bind_sys_id
+    f += struct.pack('B', 0)  # ext_attr
+    f += b'\0\0'  # reserved
+    f += struct.pack('<I', 0x03000000)  # mem_size
+    f += struct.pack('<I', len(data))  # md_img_size
+    f += struct.pack('<I', 0)  # rpc_sec_mem_addr
+    f += struct.pack('<I', 0x00A20000)  # dsp_img_offset
+    f += struct.pack('<I', 0x00219D1C)  # dsp_img_size
+    f += b'\0' * 88  # reserved
+    f += struct.pack('<I', 284)  # size
     return f
 
 if __name__ == "__main__":
