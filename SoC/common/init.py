404859dc9b26e5e3dc53d1dbf4e684b48ab63210
cyrozap <cyrozap@gmail.com>
2020-03-27 03:17:25 -0500

    Enable baudrate switching

--- SoC/common/init.py
+++ SoC/common/init.py
@@ -187,9 +187,16 @@ class Bmo:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('port', type=str, help="The serial port you want to connect to.")
+    parser.add_argument('-b', '--baudrate', type=int, default=115200, help="The baud rate you want to connect at. Default: 115200")
+    parser.add_argument('-s', '--baudrate-next', type=int, default=115200, help="The baud rate you want to switch to. Default: 115200")
     args = parser.parse_args()
 
-    bmo = Bmo(args.port, debug=False)
+    bmo = Bmo(args.port, baudrate=args.baudrate, debug=False)
+    if args.baudrate_next != args.baudrate:
+        print("Switching to baudrate to {}...".format(args.baudrate_next))
+        bmo.setbaud(args.baudrate_next)
+
+        bmo = Bmo(args.port, baudrate=args.baudrate_next, debug=False)
 
     #bmo.debug = True
     #bmo.debug = False
