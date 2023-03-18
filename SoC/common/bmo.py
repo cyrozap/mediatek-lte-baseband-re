d505fbc516303e9f4d9f7fc39e4224c35620b3b1
cyrozap <cyrozap@gmail.com>
2023-03-17 21:02:28 -0500

    Fix licenses for socemu.py and bmo.py
    Unicorn is based on Qemu, which as a whole is licensed under only
    version 2 of the GPL. Since socemu.py depends on Unicorn, socemu.py
    needs to be licensed under a GPLv2-compatible license. socemu.py also
    depends on bmo.py, so to ensure that the combined work can be
    distributed under the GPLv2 bmo.py also needs to be licensed under a
    GPLv2-compatible license.
    
    Fix both of these issues by relicensing both socemu.py and bmo.py under
    version 2 or later of the GNU GPL.

--- SoC/common/bmo.py
+++ SoC/common/bmo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 # bmo.py - A library for interacting with SoCs running the serial monitor's
 # Binary MOde protocol.
@@ -7,7 +7,7 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
--- SoC/common/socemu.py
+++ SoC/common/socemu.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 # socemu.py - A tool for emulating raw binaries that passes MMIO to real
 # hardware over serial or JTAG.
@@ -7,7 +7,7 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
